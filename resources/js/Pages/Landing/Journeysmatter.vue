75e4ce34a9d14e7d9bd26c68bd9fca71f43ba48c
Marin Nikolli <sibalonat@gmail.com>
2022-12-20 00:32:15 +0100

    matterjs, before the click events

--- resources/js/Pages/Landing/Journeysmatter.vue
+++ resources/js/Pages/Landing/Journeysmatter.vue
@@ -16,7 +16,23 @@ const prop = defineProps({
 
 //variables
 let arraySpanSizes = ['2', '3', '4', '5']
-
+// let arrayClasses = [
+//     {
+//         className: "-i1",
+//     },
+//     {
+//         className: "-i2",
+//     },
+//     {
+//         className: "-i3",
+//     },
+//     {
+//         className: "-i4",
+//     },
+//     {
+//         className: "-i5",
+//     },
+// ]
 let arrSpostime = ['1', '2', '3', '4', '5', '6', '7', '8', '8', '10']
 
 let testarr = ref(6)
@@ -30,91 +46,125 @@ const startFallbox = () => {
     const Bodies = Matter.Bodies;
     const Body = Matter.Body;
     const Composite = Matter.Composite;
-    const Composites = Matter.Composites;
-    const Common = Matter.Common;
-    const Mouse = Matter.Mouse
     const MouseConstraint = Matter.MouseConstraint;
 
     const engine = Engine.create();
     const world = engine.world;
     engine.gravity.y = 1;
 
-    // console.log('this will trigger');
+    console.log('this will trigger');
 
     const fallbox = document.querySelector(".falling-scene");
-    var render = Render.create({
+
+    const render = Render.create({
         element: fallbox,
-        engine: engine,
+        engine,
         options: {
-            width: 800,
-            height: 600,
-            showAngleIndicator: true,
-            wireframes: false,
-        }
+            width: fallbox.offsetWidth,
+            height: fallbox.offsetHeight,
+        },
     });
 
-    Render.run(render);
 
-    var runner = Runner.create();
+    const runner = Runner.create();
     Runner.run(runner, engine);
 
-    var stack = Composites.stack(20, 20, 10, 5, 0, 0, function (x, y) {
-        var sides = Math.round(Common.random(1, 8));
+    // prop.tours
+    const itemArray = arrayClasses;
+    console.log(itemArray);
+    itemArray.forEach((i) => {
+        const get = document.getElementsByClassName(i.className)[0];
 
-        // round the edges of some bodies
-        var chamfer = null;
-        if (sides > 2 && Common.random() > 0.7) {
-            chamfer = {
-                radius: 10
-            };
-        }
+        get.style.opacity = 1;
+        console.log(get);
 
-        switch (Math.round(Common.random(0, 1))) {
-            case 0:
-                if (Common.random() < 0.8) {
-                    return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
-                } else {
-                    return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
+        const item = {
+            w: get.clientWidth,
+            h: get.clientHeight,
+            body: Bodies.rectangle(
+                Math.random() * window.innerWidth,
+                Math.random() * -1000,
+                get.clientWidth,
+                get.clientHeight,
+                {
+                    restitution: 0.5,
+                    angle: Math.random() * 360,
                 }
-            case 1:
-                return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
-        }
+            ),
+
+            elem: get,
+            render() {
+                const { x, y } = this.body.position;
+                this.elem.style.top = `${y - this.h / 2}px`;
+                this.elem.style.left = `${x - this.w / 2}px`;
+                this.elem.style.transform = `rotate(${this.body.angle}rad)`;
+            },
+        };
+
+        Body.rotate(item.body, Math.random() * 360);
+
+        Composite.add(world, [item.body]);
+
+        (function rerender() {
+            item.render();
+            requestAnimationFrame(rerender);
+        })();
     });
 
-    Composite.add(world, stack);
+    const ground = Bodies.rectangle(
+        fallbox.offsetWidth / 2,
+        fallbox.offsetHeight,
+        2000,
+        1,
+        {
+            isStatic: true,
+        }
+    );
 
-    Composite.add(world, [
-        // walls
-        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-        Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-    ]);
+    const left = Bodies.rectangle(
+        0,
+        fallbox.offsetHeight / 2,
+        1,
+        fallbox.offsetHeight,
+        {
+            isStatic: true,
+        }
+    );
 
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
+    const right = Bodies.rectangle(
+        fallbox.offsetWidth,
+        fallbox.offsetHeight / 2,
+        1,
+        fallbox.offsetHeight,
+        {
+            isStatic: true,
+        }
+    );
+
+    Composite.add(world, [ground, left, right]);
+
+    const mouseConstraint = MouseConstraint.create(engine, {
+        element: fallbox,
+        constraint: {
+            stiffness: 0.2,
+        },
+    });
+
+    mouseConstraint.mouse.element.removeEventListener(
+        "mousewheel",
+        mouseConstraint.mouse.mousewheel
+    );
+    mouseConstraint.mouse.element.removeEventListener(
+        "DOMMouseScroll",
+        mouseConstraint.mouse.mousewheel
+    );
 
     Composite.add(world, mouseConstraint);
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    // fit the render viewport to the scene
     Render.lookAt(render, {
         min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
+        max: { x: fallbox.offsetWidth, y: fallbox.offsetHeight },
     });
-
-
 
 }
 
--- resources/js/Pages/Landing/Journeysmatterbosh.vue
+++ resources/js/Pages/Landing/Journeysmatterbosh.vue
@@ -3,7 +3,23 @@
 import { Head, Link } from '@inertiajs/inertia-vue3';
 import { computed, onMounted, ref } from '@vue/runtime-core';
 import _ from 'lodash';
-import Matter from "matter-js";
+// import Matter from "matter-js";
+import {
+  Engine,
+  Render,
+  World,
+  Bodies,
+  Body,
+  Events,
+  Composite,
+  Composites,
+  Constraint,
+  Vertices,
+  Mouse,
+  MouseConstraint,
+  Query,
+  Common
+} from "matter-js";
 
 // heroicons
 import { FlagIcon, InformationCircleIcon, Cog6ToothIcon, ChevronRightIcon } from '@heroicons/vue/24/outline'
@@ -23,116 +39,20 @@ let testarr = ref(6)
 let arrayTochange = ref([])
 
 //methods
-const startFallbox = () => {
-    const Engine = Matter.Engine;
-    const Render = Matter.Render;
-    const Runner = Matter.Runner;
-    const Bodies = Matter.Bodies;
-    const Body = Matter.Body;
-    const Composite = Matter.Composite;
-    const Composites = Matter.Composites;
-    const Common = Matter.Common;
-    const Mouse = Matter.Mouse
-    const MouseConstraint = Matter.MouseConstraint;
 
-    const engine = Engine.create();
-    const world = engine.world;
-    engine.gravity.y = 1;
-
-    // console.log('this will trigger');
-
-    const fallbox = document.querySelector(".falling-scene");
-    var render = Render.create({
-        element: fallbox,
-        engine: engine,
-        options: {
-            width: 800,
-            height: 600,
-            showAngleIndicator: true,
-            wireframes: false,
-        }
-    });
-
-    Render.run(render);
-
-    var runner = Runner.create();
-    Runner.run(runner, engine);
-
-    var stack = Composites.stack(20, 20, 10, 5, 0, 0, function (x, y) {
-        var sides = Math.round(Common.random(1, 8));
-
-        // round the edges of some bodies
-        var chamfer = null;
-        if (sides > 2 && Common.random() > 0.7) {
-            chamfer = {
-                radius: 10
-            };
-        }
-
-        switch (Math.round(Common.random(0, 1))) {
-            case 0:
-                if (Common.random() < 0.8) {
-                    return Bodies.rectangle(x, y, Common.random(25, 50), Common.random(25, 50), { chamfer: chamfer });
-                } else {
-                    return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(25, 30), { chamfer: chamfer });
-                }
-            case 1:
-                return Bodies.polygon(x, y, sides, Common.random(25, 50), { chamfer: chamfer });
-        }
-    });
-
-    Composite.add(world, stack);
-
-    Composite.add(world, [
-        // walls
-        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-        Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-    ]);
-
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    Composite.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
-    });
-
-
-
-}
 
 onMounted(() => {
     Head, Link
     FlagIcon, InformationCircleIcon, ChevronRightIcon
+    // matterjs
+    Engine, Render, World, Bodies, Body, Events, Composite, Composites, Constraint, Vertices, Mouse, MouseConstraint, Query, Common
 
     // properties
     testarr, arraySpanSizes, arrSpostime
 
     console.log(prop.tours);
-    startFallbox();
-    // window.addEventListener("DOMContentLoaded", () => {
-    //     console.log('is it now');
-    //     startFallbox();
-    // });
 
-    console.log(Matter);
+
 
 
     for (let index = 0; index < testarr.value; index++) {
@@ -153,9 +73,7 @@ onMounted(() => {
         <div class="relative max-w-full mx-auto sm:px-6 lg:px-8 bg-virtual-blue">
             <div class="relative flex flex-col justify-center h-screen falling-scene">
 
-                <div v-for="item in arrayClasses" :key="item.className">
-                    <span :class="item.className" class="item">une</span>
-                </div>
+
                 <!-- <div class="grid w-2/3 gap-4 grid-cols-16 falling-scene"> -->
                 <!-- <div class="content-center" v-for="(item, i) in prop.tours" :key="item.slug"
                         :class="[parseInt(arrayTochange[i]) < 4 ?  `col-start-${parseInt(arrayTochange[i]) + 6}` : `col-end-${_.sample(arrSpostime) - 2}`, `col-span-${arrayTochange[i]}`]">
