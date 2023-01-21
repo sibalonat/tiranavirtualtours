f1920b6463ae3f513ca450397b755d2c48ff62ae
Marin Nikolli <sibalonat@gmail.com>
2023-01-21 17:53:48 +0100

    some changes

--- resources/js/Pages/Landing/SingleTour.vue
+++ resources/js/Pages/Landing/SingleTour.vue
@@ -331,10 +331,13 @@ watchEffect(() => {
                 </div>
                 <div class="overflow-y-hidden"
                     :class="!smAndLarger ? 'relative grow h-60' : 'absolute z-[1000] w-1/3 h-full bg-gray-circles'">
-                    <div class="grid w-11/12 grid-cols-4 mt-20 overflow-hidden">
-                        <p class="col-span-3 px-5 pb-2 my-auto text-3xl font-semibold text-start text-virtual-blue">
-                            {{ languageChange === 'AL' ? 'Rreth rrugëtimit' : 'About this tour' }}
-                        </p>
+                    <div class="grid w-11/12 grid-cols-4 mt-20 mb-8 overflow-hidden">
+                        <Link class="grid grid-cols-7 col-span-3" :href="route('landing.tours')">
+                            <ChevronLeftIcon class="col-start-2 my-auto stroke-2 text-virtual-blue w-7 h-7" />
+                            <p class="col-span-5 col-start-3 my-auto text-3xl font-semibold text-start text-virtual-blue">
+                                {{ languageChange === 'AL' ? 'Rreth rrugëtimit' : 'About this tour' }}
+                            </p>
+                        </Link>
                         <button class="h-8 px-3 ml-auto mr-5 text-white bg-virtual-blue rounded-xl"
                             @click="languageChange === 'AL' ? languageChange = 'EN' : languageChange = 'AL'">
                             {{ languageChange }}
