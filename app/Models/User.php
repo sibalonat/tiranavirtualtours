2b91d50d3ab97dcb6061d0dcb0080499248ea945
Marin Nikolli <sibalonat@gmail.com>
2023-03-21 00:32:45 +0100

    .

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Settings;
+
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
